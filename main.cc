e93976a9e6c9ccffce48fc8c180b03f3ff17a679
jerry_a5e <xwyz8080@outlook.com>
2025-10-30 07:29:59 +0000

    fix gitignore

--- main.cc
+++ main.cc
@@ -1,11 +1,20 @@
 #include <drogon/drogon.h>
+
 int main() {
-    //Set HTTP listener address and port
+    // 设置 HTTP 监听地址和端口
     drogon::app().addListener("0.0.0.0", 5555);
-    //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-    //drogon::app().loadConfigFile("../config.yaml");
-    //Run HTTP framework,the method will block in the internal event loop
+
+    // 注册根路径 handler
+    drogon::app().registerHandler("/",
+        [](const drogon::HttpRequestPtr &req,
+           std::function<void (const drogon::HttpResponsePtr &)> &&callback) {
+            auto resp = drogon::HttpResponse::newHttpResponse();
+            resp->setBody("Hello World");
+            callback(resp);
+        });
+
+    // 运行框架
     drogon::app().run();
     return 0;
 }
+
